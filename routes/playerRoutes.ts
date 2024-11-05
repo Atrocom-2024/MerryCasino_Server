38a852e947dabed386b94d402fd8508457e09e79
TaeWooKim-SCH <zop1234@hanmail.net>
2024-11-05 14:33:47 +0900

    feat: 제품 구매 기능

--- routes/playerRoutes.ts
+++ routes/playerRoutes.ts
@@ -3,8 +3,14 @@ import { getPlayer, createPlayer, updatePlayer } from '../controllers/playerCont
 
 const playerRoutes = Router();
 
+// /api/players
 playerRoutes.post('/players', createPlayer); // 플레이어 생성
+
+// /api/players/:player_id
 playerRoutes.get('/players/:player_id', getPlayer); // 플레이어 정보 조회
 playerRoutes.patch('/players/:player_id', updatePlayer); // 플레이어 정보 수정
+
+// /api/players/:player_id/purchase
+playerRoutes.post('/players/:player_id/purchase');
 
 export default playerRoutes;
