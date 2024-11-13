78a946f0cdee8d5f46c9be295cec1fc69959f3eb
TaeWooKim-SCH <zop1234@hanmail.net>
2024-11-13 15:43:45 +0900

    fix: 필요 없는 코드 수정

--- controllers/roomController.ts
+++ controllers/roomController.ts
@@ -29,10 +29,22 @@ export const getPayout = (req: Request, res: Response) => {
     return;
   }
 
-  // res.status(200).json({
-  //   resultPayout: roomList[roomNumber].resultplusPercent
-  // });
   res.send(roomList[roomNumber].resultplusPercent.toString());
+  return;
+};
+
+// 특정 room의 targetPayout 조회
+export const getTargetPayout = (req: Request, res: Response) => {
+  console.log("특정 room targetPayout 조회");
+
+  const roomNumber = parseInt(req.params.roomNumber, 10);
+
+  if (!roomList[roomNumber]) {
+    res.status(404).send('Room not found');
+    return;
+  }
+
+  res.send(roomList[roomNumber].targetPayout.toString());
   return;
 };
 
@@ -67,20 +79,5 @@ export const updateBet = (req: Request, res: Response) => {
 
   roomList[roomNumber].totalBet += betCoin;
   res.send(`Total bet updated for room ${roomNumber}`);
-  return;
-};
-
-// 특정 room의 targetPayout 조회
-export const getTargetPayout = (req: Request, res: Response) => {
-  console.log("특정 room targetPayout 조회");
-  
-  const roomNumber = parseInt(req.params.roomNumber, 10);
-
-  if (!roomList[roomNumber]) {
-    res.status(404).send('Room not found');
-    return;
-  }
-
-  res.send(roomList[roomNumber].targetPayout.toString());
   return;
 };
--- routes/roomRoutes.ts
+++ routes/roomRoutes.ts
@@ -9,29 +9,10 @@ import {
 
 const roomRoutes = Router();
 
-roomRoutes.get('/:roomNumber/payout-return', getPayoutReturn);
-// roomRoutes.get('/Payoutreturn/:roomNumber', (req, res) => {
-//   res.send(roomList[req.params.roomNumber].returnEventValue.toString());
-// });
-
 roomRoutes.get('/:roomNumber/payout', getPayout);
-// roomRoutes.get('/Payout/:roomNumber', (req, res) => {
-//   res.send(roomList[req.params.roomNumber].resultplusPercent.toString());
-// });
-
-roomRoutes.post('/:roomNumber/win', updateWin);
-// roomRoutes.get('/Win/:roomNumber/:betCoin', (req, res) => {
-//   roomList[req.params.roomNumber].totalPayout += req.params.betCoin;
-// });
-
-roomRoutes.post('/:roomNumber/bet', updateBet);
-// roomRoutes.get('/Wheel/:roomNumber/:betCoin', (req, res) => {
-//   roomList[req.params.roomNumber].totalBet += req.params.betCoin;
-// });
-
+roomRoutes.get('/:roomNumber/payout-return', getPayoutReturn);
 roomRoutes.get('/:roomNumber/target-payout', getTargetPayout);
-// roomRoutes.get('/targetPayout/:roomNumber', (req, res) => {
-//   res.send(roomList[req.params.roomNumber].targetPayout.toString());
-// });
+roomRoutes.post('/:roomNumber/win', updateWin);
+roomRoutes.post('/:roomNumber/bet', updateBet);
 
 export default roomRoutes;
